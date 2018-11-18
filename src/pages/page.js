2195816737d55428e269448f1a32c29b9a870553
Inerc <inerc@mail.ru>
2018-11-18 21:33:40 +0300

    fix errors 2

--- src/pages/page.js
+++ src/pages/page.js
@@ -15,7 +15,7 @@ const IndexPage = ({ data }) => {
   return (
     <div>
       <Navigation />
-      <Header title={blog.title.text} description={'How to Lose Weight'} image={blog.image.url} />
+      <Header title={blog && blog.title } description={'How to Lose Weight'} image={blog && blog.image} />
       <article>
         <div className="container">
           <div className="row">
