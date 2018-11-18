f5663634a0221f38dbbae803a587deffe4ffd1ae
ilovetraffic <aidarfreelancer@gmail.com>
2018-11-18 23:35:22 +0300

    fix page

--- src/pages/page.js
+++ src/pages/page.js
@@ -3,13 +3,13 @@ import { graphql } from 'gatsby'
 import Navigation from '../components/navigation'
 import Header from '../components/header'
 import Footer from '../components/footer'
-
+import ReactPlayer from 'react-player'
 
 
 
 let renderContent = (data) => {
 
-  if(data.media_type =='img'){
+  if (data.media_type == 'img') {
 
     return (
       <img src={data.media} alt={data.title}></img>
@@ -17,11 +17,12 @@ let renderContent = (data) => {
 
   }
 
-  if(data.media_type =='video'){
+  if (data.media_type == 'video') {
 
     return (
-      <video src={data.media} />
-    
+
+      <ReactPlayer url={data.media} playing />
+
     )
 
   }
@@ -36,13 +37,13 @@ const IndexPage = ({ data }) => {
   return (
     <div>
       <Navigation />
-      <Header title={blog && blog.title }  image={'https://ichef.bbci.co.uk/news/660/cpsprodpb/EFCB/production/_104378316_a9b18b4c-5092-4089-b16a-a6228bac4c85.jpg'} />
+      <Header title={blog && blog.title} image={'https://ichef.bbci.co.uk/news/660/cpsprodpb/EFCB/production/_104378316_a9b18b4c-5092-4089-b16a-a6228bac4c85.jpg'} />
       <article>
         <div className="container">
           <div className="row">
             <div className="col-lg-8 col-md-10 mx-auto">
-            {renderContent(blog)}
-            <div dangerouslySetInnerHTML={{ __html:blog.content}} />
+              {renderContent(blog)}
+              <div dangerouslySetInnerHTML={{ __html: blog.content }} />
             </div>
           </div>
         </div>
