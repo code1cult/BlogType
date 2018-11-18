06917ff42200b68961dadf44818fd5802fa25bc0
ilovetraffic <aidarfreelancer@gmail.com>
2018-11-18 22:18:56 +0300

    fix page

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/home/ilovetraffic/BlogType/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/home/ilovetraffic/BlogType/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
